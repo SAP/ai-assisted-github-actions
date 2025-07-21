ab8f1eb5e1fad6ba3a3a76e8cd43c66c58bc743f
Daniel Richter <github@crftartsc.de>
2025-07-21 18:40:46 +0200

    Fix optional chaining in hunk reader and enforce non-null assertions in main

--- pr-review/src/hunk-reader.ts
+++ pr-review/src/hunk-reader.ts
@@ -34,43 +34,50 @@ export function resolveHunkReferencesInComments(comments: AiReview["comments"], 
       core.warning(`Could not find file for comment on ${comment.path}, start ${comment.start}, end ${comment.end}, ${comment.comment}, skipping.`)
     } else {
       const hunkChangeMap = currentFile.chunks.flatMap(hunk => hunk.changes.map(change => ({ change, hunk })))
-      let { change: startChange, hunk: startHunk } = hunkChangeMap[comment.start - 1] // eslint-disable-line prefer-const
-      let { change: endChange, hunk: endHunk } = hunkChangeMap[comment.end - 1] // eslint-disable-line prefer-const
+      const startEntry = hunkChangeMap[comment.start - 1]
+      const endEntry = hunkChangeMap[comment.end - 1]
 
-      if (!startHunk) {
-        core.warning(`Could not find hunk for comment on ${comment.path}, start ${comment.start}, end ${comment.end}, ${comment.comment}, skipping.`)
+      if (!startEntry || !endEntry) {
+        core.error(`Could not find hunk for comment on ${comment.path}, start ${comment.start}, end ${comment.end}, ${comment.comment}, skipping.`)
       } else {
-        if (startHunk !== endHunk) endChange = startHunk.changes.at(-1)!
+        const { change: startChange, hunk: startHunk } = startEntry
+        let { change: endChange, hunk: endHunk } = endEntry // eslint-disable-line prefer-const
 
-        const startSide = startChange.type !== "del" ? "RIGHT" : "LEFT"
-        const endSide = endChange.type !== "del" ? "RIGHT" : "LEFT"
+        if (!startHunk) {
+          core.warning(`Could not find hunk for comment on ${comment.path}, start ${comment.start}, end ${comment.end}, ${comment.comment}, skipping.`)
+        } else {
+          if (startHunk !== endHunk) endChange = startHunk.changes.at(-1)!
 
-        // get start line of the actual comment
-        let start: number
-        if (startChange.type === "normal") {
-          start = startChange.ln2
-        } else if (startChange.type === "add" || startChange.type === "del") {
-          start = startChange.ln
-        } else throw new Error(`Unknown change type.`)
+          const startSide = startChange.type !== "del" ? "RIGHT" : "LEFT"
+          const endSide = endChange.type !== "del" ? "RIGHT" : "LEFT"
 
-        // get end line of the actual comment
-        let end: number
-        if (endChange.type === "normal") {
-          end = endChange.ln2
-        } else if (endChange.type === "add" || endChange.type === "del") {
-          end = endChange.ln
-        } else throw new Error(`Unknown change type.`)
-        // make sure start and end are within the hunk
-        end = Math.min(end, endSide === "RIGHT" ? startHunk.newStart + startHunk.newLines - 1 : startHunk.oldStart + startHunk.oldLines - 1)
+          // get start line of the actual comment
+          let start: number
+          if (startChange.type === "normal") {
+            start = startChange.ln2
+          } else if (startChange.type === "add" || startChange.type === "del") {
+            start = startChange.ln
+          } else throw new Error(`Unknown change type.`)
 
-        result.push({
-          path: comment.path,
-          start_side: startSide !== endSide ? startSide : undefined, // only set start_side if it is a multi-line comment
-          side: startSide !== endSide ? endSide : startSide,
-          start_line: start !== end && start < end ? start : undefined, // only set start_line if it is a multi-line comment, start must be less than end
-          line: start !== end && start < end ? end : start,
-          body: comment.comment,
-        })
+          // get end line of the actual comment
+          let end: number
+          if (endChange.type === "normal") {
+            end = endChange.ln2
+          } else if (endChange.type === "add" || endChange.type === "del") {
+            end = endChange.ln
+          } else throw new Error(`Unknown change type.`)
+          // make sure start and end are within the hunk
+          end = Math.min(end, endSide === "RIGHT" ? startHunk.newStart + startHunk.newLines - 1 : startHunk.oldStart + startHunk.oldLines - 1)
+
+          result.push({
+            path: comment.path,
+            start_side: startSide !== endSide ? startSide : undefined, // only set start_side if it is a multi-line comment
+            side: startSide !== endSide ? endSide : startSide,
+            start_line: start !== end && start < end ? start : undefined, // only set start_line if it is a multi-line comment, start must be less than end
+            line: start !== end && start < end ? end : start,
+            body: comment.comment,
+          })
+        }
       }
     }
   })
